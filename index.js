3234d5cdc6fa86fab67fe65d3dca16135d4c4200
git-bash <test@w3schools.com>
2023-12-29 17:35:03 +0530

    Setting server side rendering with help of ejs as templating engine including partials

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const PORT = process.env.PORT || 3000;
+const path = require('path')
 
 const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve("./views"));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 
 
