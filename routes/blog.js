fa8d0b4dbd7133c26752e114f88a443b48e213f4
git-bash <test@w3schools.com>
2023-12-30 15:50:52 +0530

    making blog template

--- routes/blog.js
+++ routes/blog.js
@@ -35,5 +35,14 @@ router.post('/', upload.single('coverImage') , async (req,res) => {
     return res.redirect(`/blog/${blog._id}`);
 })
 
+router.get('/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
+    console.log(blog)
+    return res.render('blog', {
+      user: req.user,
+      blog,
+    })
+})
+
 
 module.exports = router;
